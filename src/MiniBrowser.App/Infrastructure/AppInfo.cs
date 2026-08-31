5d01835b23990abdf32fa9a2b36333ce894b8259
zhuchengxue <zhuchengxue@gmail.com>
2026-08-31 14:55:25 +0800

    Bump version to 0.5.0

--- src/MiniBrowser.App/Infrastructure/AppInfo.cs
+++ src/MiniBrowser.App/Infrastructure/AppInfo.cs
@@ -3,7 +3,7 @@ namespace MiniBrowser.App.Infrastructure;
 public static class AppInfo
 {
     public const string ProductName = "MiniBrowser";
-    public const string Version = "0.4.9";
+    public const string Version = "0.5.0";
     public const string RepositoryOwner = "zhuchengxue";
     public const string RepositoryName = "MiniBrowser";
     public const string PortableAssetName = "MiniBrowser-Portable.zip";
